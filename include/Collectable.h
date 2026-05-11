f071ae158d522b004a9adfdbf75383f3630c3ba2
Uirajan Jose da Silva <ujs@WINDOWS.CIN.UFPE.BR>
2026-05-11 19:56:58 -0300

    criação da classe de coletaveis

--- include/Collectable.h
+++ include/Collectable.h
@@ -1,1 +1,22 @@
+class Collectable
+{
+private:
+    /* data */
+public:
+    bool appear(){
+
+    }
+
+
+    Collectable(/* args */);
+    virtual ~Collectable();
+};
+
+Collectable::Collectable(/* args */)
+{
+}
+
+Collectable::~Collectable()
+{
+}
 
