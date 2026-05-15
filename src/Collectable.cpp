7dd75e81c771c2a246e95525727204ba98d7e984
Uirajan <ujs@cin.ufpe.br>
2026-05-15 18:19:54 -0300

    Update Collectable.cpp

--- src/Collectable.cpp
+++ src/Collectable.cpp
@@ -1,2 +1,7 @@
+#include <iostream>
 #include "../include/Collectable.h"
-
+#include <cstdlib>
+/*decide onde o coletavel vai nascer, baseado em qual posição do vetor da zona da instancia,atraves da geraça~o de um numwero aleatório de 0 a 12*/
+int Collectable::appear(){
+  
+}
