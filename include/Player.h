2a7f655746e853c76a0f9a45abe713916dbd8260
Felipe Melo <248579698+felipe-melo-cin@users.noreply.github.com>
2026-05-15 16:10:51 -0300

    Add Player class - Player instance acts as the game's main character

--- include/Player.h
+++ include/Player.h
@@ -1,1 +1,20 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
 
+#include "Entity.h"
+
+class Player : public Entity {
+
+    private:
+
+        int maxVida = 8;
+        bool puloDuplo = false;
+    
+    public:
+
+        Player();
+        ~Player() {}
+
+};
+
+#endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,6 @@
+#include "../include/Player.h"
 
+Player::Player() {
+    set_vida(maxVida);
+    set_camada(CHAO);
+}
