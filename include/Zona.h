1091c9e9f0a8ec8b822fb58a3359be49ace0b469
Felipe Melo <248579698+felipe-melo-cin@users.noreply.github.com>
2026-05-17 02:48:54 -0300

    Add Zona class - Game was sectioned into zones - Each zone has its own set of Collectables, Enemies and Platforms

---
+++ include/Zona.h
@@ -0,0 +1,45 @@
+#ifndef _ZONA_H_
+#define _ZONA_H_
+
+#include <vector>
+
+#include "Coletavel.h"
+#include "Inimigo.h"
+#include "Plataforma.h"
+
+class Zona {
+
+    private:
+
+        // Gera vetores contendo os coletáveis, inimigos e plataformas da zona
+        std::vector<Coletavel*> coletaveis;
+        std::vector<Inimigo*> inimigos;
+        std::vector<Plataforma*> plataformas;
+    
+    public:
+
+        Zona() {};
+        ~Zona() {
+            for (int i = 0; i < coletaveis.size(); ++i) { delete coletaveis.back(); coletaveis.pop_back(); }
+            for (int i = 0; i < inimigos.size(); ++i) { delete inimigos.back(); inimigos.pop_back(); }
+            for (int i = 0; i < plataformas.size(); ++i) { delete plataformas.back(); plataformas.pop_back(); }
+        };
+
+        // Instancia um novo membro da classe para o vetor
+        void carregar_coletavel(Coletavel* colPtr) { coletaveis.push_back(colPtr); }
+        void carregar_inimigo(Inimigo* inimPtr) { inimigos.push_back(inimPtr); }
+        void carregar_plataforma(Plataforma* platPtr) { plataformas.push_back(platPtr); }
+
+        // Busca um membro da classe armazenado no vetor por índice
+        Coletavel* get_coletavel(int index) { return coletaveis.at(index); }
+        Inimigo* get_inimigo(int index) { return inimigos.at(index); }
+        Plataforma* get_plataforma(int index) { return plataformas.at(index); }
+
+        // Destrói o membro da classe armazenado no vetor
+        void eliminar_coletavel(int index) { delete get_coletavel(index); coletaveis.erase(coletaveis.begin() + index); }
+        void eliminar_inimigo(int index) { delete get_inimigo(index); inimigos.erase(inimigos.begin() + index); }
+        void eliminar_plataforma(int index) { delete get_inimigo(index); plataformas.erase(plataformas.begin() + index); }
+
+};
+
+#endif
