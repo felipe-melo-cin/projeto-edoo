c8c5bd8dc4aa8e49af628ad80142e37133aff4ca
Felipe Melo <248579698+felipe-melo-cin@users.noreply.github.com>
2026-05-17 02:46:38 -0300

    Add Plataforma class - Implements platforms from the base game

---
+++ include/Plataforma.h
@@ -0,0 +1,18 @@
+#ifndef _PLATAFORMA_H_
+#define _PLATAFORMA_H_
+
+#include "Camada.h"
+
+class Plataforma {
+
+    private:
+
+        enum Camada camada;
+    
+    public:
+
+        Plataforma(enum Camada camada);
+
+};
+
+#endif
---
+++ src/Plataforma.cpp
@@ -0,0 +1,3 @@
+#include "../include/Plataforma.h"
+
+Plataforma::Plataforma(enum Camada camada) : camada(camada) {}
