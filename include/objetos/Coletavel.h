b411d42f46ce8daeb9ab8f8407df217d306320aa
Felipe Melo <248579698+felipe-melo-cin@users.noreply.github.com>
2026-05-22 22:48:32 -0300

    Fix refactor errors - Bad refactor removed essential headers from Coletavel.cpp causing compilation errors - Headers were replenished to the state before code cleanup

--- include/objetos/Coletavel.h
+++ include/objetos/Coletavel.h
@@ -3,8 +3,6 @@
 
 #include <string>
 
-#include "gerador_aleatoriedade.h"
-
 class Coletavel {
 
     private:
--- src/objetos/Coletavel.cpp
+++ src/objetos/Coletavel.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <random> // Biblioteca muito melhor para gerar aleatórios em relação as tranqueiras do C.
+#include <ctime>  // Semente para deixar as saídas pseudoaleatórias
 
 #include "../../include/objetos/Coletavel.h"
 
-int Coletavel::gerarPosicao(){
+int Coletavel::gerarPosicao() {
 
     static std::mt19937 gerador(std::time(nullptr)); // Gerador utilizando o unix timestamp para valores pseudoaleatórios únicos
 
